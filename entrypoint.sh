1cadd03f316feaee5587292f7fa53a1b86c1b2e4
Sébastien Le Callonnec <sebastien@weblogism.com>
2024-02-05 11:21:58 +0000

    Try debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+echo $1
+awk '{print substr($0, 5, 5)}' <<< $2
 curl --request PATCH \
      --header 'Authorization: Bearer $2' \
      --header 'Content-type: application/json' \
