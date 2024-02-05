da2d33ab406badb673866f1ffb0f4f78c0fffc56
Sébastien Le Callonnec <sebastien@weblogism.com>
2024-02-05 09:37:20 +0000

    Fix script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 curl --request PATCH \
-     --header 'Authorization: Bearer ${{ INPUT_TOKEN }}' \
+     --header 'Authorization: Bearer ${INPUT_TOKEN}' \
      --header 'Content-type: application/json' \
      --header 'User-Agent: Github-Action' \
      --url ${INPUT_RELEASE} \
